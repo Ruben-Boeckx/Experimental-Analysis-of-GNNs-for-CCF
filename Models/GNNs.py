2df3e736f77625c03a12340f7bad9b3d6e2a09b7
Ruben Boeckx <boeckxruben@gmail.com>
2024-06-10 08:51:33 +0200

    update

--- Models/GNNs.py
+++ Models/GNNs.py
@@ -1,7 +1,7 @@
-from torch_geometric.nn import SAGEConv, GCNConv, GATConv
+from torch_geometric.nn import SAGEConv, GCNConv, GATConv, Linear
 import torch
 
-# GraphSAGE for node classification
+# GraphSAGE for node classification, OK
 class GraphSAGE(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels, dropout_prob):
         super(GraphSAGE, self).__init__()
@@ -15,7 +15,7 @@ class GraphSAGE(torch.nn.Module):
         x = self.conv2(x, edge_index)
         return x
     
-# Graph Convolutional Network for node classification
+# Graph Convolutional Network for node classification, FIX
 class GCN(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels, dropout_prob):
         super(GCN, self).__init__()
@@ -29,16 +29,17 @@ class GCN(torch.nn.Module):
         x = self.conv2(x, edge_index)
         return x
 
-# Graph Attention Network for node classification
+# Graph Attention Network for node classification, OK
 class GAT(torch.nn.Module):
-    def __init__(self, hidden_channels, out_channels, dropout_prob, heads=1):
-        super(GAT, self).__init__()
-        self.conv1 = GATConv((-1, -1), hidden_channels, heads=heads)
-        self.conv2 = GATConv((-1, -1), out_channels, heads=1)
-        self.dropout = torch.nn.Dropout(p=dropout_prob)
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GATConv((-1, -1), hidden_channels, add_self_loops=False)
+        self.lin1 = Linear(-1, hidden_channels)
+        self.conv2 = GATConv((-1, -1), out_channels, add_self_loops=False)
+        self.lin2 = Linear(-1, out_channels)
 
     def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index).relu()
-        x = self.dropout(x)
-        x = self.conv2(x, edge_index)
+        x = self.conv1(x, edge_index) + self.lin1(x)
+        x = x.relu()
+        x = self.conv2(x, edge_index) + self.lin2(x)
         return x
