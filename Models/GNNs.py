5fa8df2beae33555ac55e48c1b6699f4355989b9
Ruben Boeckx <boeckxruben@gmail.com>
2024-06-20 22:19:09 +0200

    further develop GNNs

--- Models/GNNs.py
+++ Models/GNNs.py
@@ -1,6 +1,9 @@
-import torch
+from re import search
+import torch.nn as nn 
+import torch 
+from torch_geometric.nn import GCNConv, GATv2Conv,GINEConv,GraphSAGE, SAGEConv, GATConv
+from torch.nn import Linear, LayerNorm
 import torch.nn.functional as F
-from torch_geometric.nn import SAGEConv, GATConv, Linear
 
 # GraphSAGE for node classification, OK
 class GraphSAGE(torch.nn.Module):
@@ -30,3 +33,42 @@ class GAT(torch.nn.Module):
         x = x.relu()
         x = self.conv2(x, edge_index) + self.lin2(x)
         return x
+
+# copied from Tiukhova paper, will probably need to be modified
+class GATs(torch.nn.Module): 
+    def __init__(self,input_dim, embedding_dim,output_dim,edge_dim,heads, n_layers, dropout_rate, **kwargs) -> None:
+        super().__init__()
+        self.dropout_rate = dropout_rate
+        self.gat1 = GATv2Conv(input_dim, embedding_dim, heads=heads,edge_dim = edge_dim) # dim_h * num heads
+        self.layer_norm1 = LayerNorm(embedding_dim*heads, elementwise_affine=True)
+        self.GAT_list = torch.nn.ModuleList([GATv2Conv(embedding_dim*heads, embedding_dim, heads=heads,edge_dim = edge_dim)  for _ in range(n_layers-2)])
+        self.gat2 = GATv2Conv(embedding_dim*heads, output_dim, heads=1, edge_dim = edge_dim)
+        self.layer_norm2 = LayerNorm(output_dim, elementwise_affine=True)
+        self.dropout = nn.Dropout(self.dropout_rate)
+
+    def forward(self, inp, edge_index,edge_feats):
+
+        h = self.gat1(inp, edge_index,edge_attr = edge_feats)
+        h = F.elu(h)
+        h = self.dropout(h)
+        for l in self.GAT_list:
+            h = l(h, edge_index,edge_attr = edge_feats)
+            h = self.layer_norm1(h)
+            h = F.elu(h)
+            h = self.dropout(h)
+        h = self.gat2(h, edge_index, edge_feats)
+        h = self.layer_norm2(h)
+        h = F.elu(h)
+        h = self.dropout(h)
+        return h
+
+def get_GNN(gnn ):
+    
+    if gnn == 'GAT':
+        return GATs
+    elif gnn == 'GIN':
+        return GINs
+    elif gnn == 'SAGE':
+        return SAGEs
+    else: 
+        return GCNs
