e7c965c9ddef2943c76a20de6d4b1b2cef4ef1ba
Ruben Boeckx <boeckxruben@gmail.com>
2024-07-02 11:01:17 +0200

    create GAT model

--- Models/GNNs.py
+++ Models/GNNs.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
-from torch_geometric.nn import SAGEConv, Linear, GATConv
+from torch_geometric.nn import SAGEConv, Linear, GATConv, GATv2Conv
     
 
 class GraphSAGE2(torch.nn.Module):
@@ -43,6 +43,49 @@ class GraphSAGE2(torch.nn.Module):
                 h = F.relu(h)
                 h = self.dropout(h)
             h = self.sage2(h, edge_index)
+        out = self.out(h)
+        
+        return out
+    
+class GAT2(torch.nn.Module):
+    def __init__(self, 
+                 hidden_dim: int,
+                 embedding_dim: int,
+                 output_dim: int,
+                 num_layers: int,
+                 dropout_rate: float,
+                 heads: int):
+        super(GAT2, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
+        self.output_dim = output_dim
+        self.dropout_rate = dropout_rate
+        self.dropout = nn.Dropout(dropout_rate)
+        self.heads = heads
+        self.num_layers = num_layers
+        self.gat_layers = nn.ModuleList()
+        
+        if num_layers == 1:
+            self.gat1 = GATv2Conv((-1, -1), embedding_dim, heads=heads, concat=False, add_self_loops=False)
+        else:
+            self.gat1 = GATv2Conv((-1, -1), hidden_dim, heads=heads, add_self_loops=False)
+            for _ in range(num_layers - 2):
+                self.gat_layers.append(GATv2Conv((-1, -1), hidden_dim, heads=heads, add_self_loops=False))
+            self.gat2 = GATv2Conv((-1, -1), embedding_dim, heads=heads, concat=False, add_self_loops=False)
+
+        self.out = Linear(embedding_dim, output_dim)
+
+    def forward(self, x, edge_index):
+        h = self.gat1(x, edge_index)
+        h = F.relu(h)
+        h = self.dropout(h)
+        if self.num_layers > 1:
+            for layer in self.gat_layers:
+                h = layer(h, edge_index)
+                h = F.relu(h)
+                h = self.dropout(h)
+            h = self.gat2(h, edge_index)
         out = self.out(h)
         
         return out
