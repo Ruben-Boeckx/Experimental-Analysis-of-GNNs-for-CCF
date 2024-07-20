1344058e6b118e0ed8f3fd1fd6d8d478bd8fe468
Ruben Boeckx <boeckxruben@gmail.com>
2024-07-20 21:41:41 +0200

    deletions

--- Models/GNNs.py
+++ Models/GNNs.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
-from torch_geometric.nn import SAGEConv, Linear, GATConv, GATv2Conv
+from torch_geometric.nn import SAGEConv, Linear, GATConv
 
 class GraphSAGE(torch.nn.Module):
     def __init__(self, 
@@ -93,68 +93,6 @@ class GAT(torch.nn.Module):
             
             h = self.gat2(h, edge_index, edge_attr=edge_attr) + self.lin2(h)
         
-        out = self.out(h)
-        
-        return out
-
-class GAT2(torch.nn.Module):
-    def __init__(self, hidden_channels, out_channels):
-        super().__init__()
-        self.conv1 = GATConv((-1, -1), hidden_channels, add_self_loops=False)
-        self.lin1 = Linear(-1, hidden_channels)
-        self.conv2 = GATConv((-1, -1), out_channels, add_self_loops=False)
-        self.lin2 = Linear(-1, out_channels)
-
-    def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index) + self.lin1(x)
-        x = x.relu()
-        x = self.conv2(x, edge_index) + self.lin2(x)
-        return x
-
-class GAT3(torch.nn.Module):
-    def __init__(self,
-                 hidden_dim: int,
-                 embedding_dim: int,
-                 output_dim: int,
-                 num_layers: int,
-                 dropout_rate: float,
-                 add_self_loops: bool = False):
-        super(GAT3, self).__init__()
-
-        self.hidden_dim = hidden_dim
-        self.embedding_dim = embedding_dim
-        self.output_dim = output_dim
-        self.dropout_rate = dropout_rate
-        self.dropout = nn.Dropout(dropout_rate)
-        self.num_layers = num_layers
-        self.add_self_loops = add_self_loops
-        self.gat_layers = nn.ModuleList()
-        self.lin_layers = nn.ModuleList()
-
-        if num_layers == 1:
-            self.gat1 = GATConv((-1, -1), embedding_dim, add_self_loops=add_self_loops)
-            self.lin1 = Linear(-1, embedding_dim)
-        else:
-            self.gat1 = GATConv((-1, -1), hidden_dim, add_self_loops=add_self_loops)
-            self.lin1 = Linear(-1, hidden_dim)
-            for _ in range(num_layers - 2):
-                self.gat_layers.append(GATConv((-1, -1), hidden_dim, add_self_loops=add_self_loops))
-                self.lin_layers.append(Linear(-1, hidden_dim))
-            self.gat2 = GATConv((-1, -1), embedding_dim, add_self_loops=add_self_loops)
-            self.lin2 = Linear(-1, embedding_dim)
-
-        self.out = Linear(embedding_dim, output_dim)
-
-    def forward(self, x, edge_index):
-        h = self.gat1(x, edge_index) + self.lin1(x)
-        h = F.relu(h)
-        h = self.dropout(h)
-        if self.num_layers > 1:
-            for gat_layer, lin_layer in zip(self.gat_layers, self.lin_layers):
-                h = gat_layer(h, edge_index) + lin_layer(h)
-                h = F.relu(h)
-                h = self.dropout(h)
-            h = self.gat2(h, edge_index) + self.lin2(h)
         out = self.out(h)
         
         return out
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# A Benchmark of Network Learning Techniques
-This repository contains the Python code developed for the benchmarking process in my master's thesis at KU Leuven.
+## An Experimental Analysis of Graph Neural Networks for Credit Card Fraud Detection
+This repository contains the Python code developed for setting up the experimental analysis on GNNs in my master's thesis at KU Leuven.
